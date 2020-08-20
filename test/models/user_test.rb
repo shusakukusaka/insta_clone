a5e69e4b43df842a9f248eb86952c68201534214
Ubuntu <ubuntu@ip-172-31-20-53.ap-northeast-1.compute.internal>
2020-08-20 14:15:01 +0000

    model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+    @user = User.new(name: "shusaku", email: "shusakukusaka@icloud.com", password: "foobar")
+    end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+
+
+test "email should be present" do
+  @user.email = "  "
+  assert_not @user.valid?
+end
 end
