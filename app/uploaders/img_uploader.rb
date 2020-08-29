190fa3c898e081ed5aaa6d75a222ff582cf718e9
shusakukusaka <shusakukusaka@icloud.com>
2020-08-29 15:49:59 +0000

    resize

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -3,12 +3,7 @@ class ImgUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [200, 200]
   # Choose what kind of storage to use for this uploader:
-  
-  if Rails.env.production?
-    storage :fog
-  else
     storage :file
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
